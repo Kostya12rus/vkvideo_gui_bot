ceddb24c50d6939cb9c272dcb9070557a3e2db7d
Kostya <mkg97@yandex.ru>
2026-03-08 20:48:35 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.13",
+    python_requires=">=3.10",
     license="MIT",
     keywords="desktop application vkvideo bot gui",
     project_urls={
